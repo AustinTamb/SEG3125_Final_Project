0705df5588dc06bad5abbf031c981686a6005f2c
AustinTamb <atamb073@uottawa.ca>
2018-07-12 15:24:59 -0400

    Added stuff, gave up on trying to add favorites

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ $(document).ready(function () {
         searchChanged();
     });
 
+    $("#btn-login").on("click", function(){
+        $("#loginModal").modal();
+    });
+
     // When the user changes the keyword category
     $("#search_category", "#search_recipe").on("change", function () {
         searchChanged();
@@ -251,3 +255,50 @@ function replaceFractions(to_parse) {
     }
     return to_parse;
 }
+
+
+
+function login(usr){
+    // TODO: Get user id to be stored in cookie (Making getting from database easier)
+    // TODO: 
+    setCookie("userid", usr, 30);
+
+}
+
+/*
+Code below comes from : https://www.w3schools.com/js/js_cookies.asp
+*/
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+} 
